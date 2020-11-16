2d82a36f47c42ad16692a44007e111885c28fd9e
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-16 16:20:52 +0530

    useState using prevState

--- src/components/first/Counter.js
+++ src/components/first/Counter.js
@@ -2,9 +2,30 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+
+  //   bad practice, will not work
+  //   const incFive = () => {
+  //     for (let i = 0; i < 5; i++) {
+  //       setCount(count + 1);
+  //     }
+  //   };
+
+  //Whenever we want to change the state based on previous state, always use fun in setState which has access
+  //to previos state
+  const incFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prevCount => prevCount + 1);
+    }
+  };
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Count : {count}</button>
+      {count}
+      <br />
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Count +1
+      </button>
+      <button onClick={incFive}>Increment +5</button>
     </div>
   );
 }
